91fcdc5ee93e30df1ba675b4d3d3a7f3918c91ac
Beondel <bengmacmillan@gmail.com>
2017-02-10 23:28:05 -0800

    hooked up UIkit

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // cards
+    @IBOutlet weak var rightCard: UIImageView!
+    @IBOutlet weak var leftCard: UIImageView!
+    // scores
+    @IBOutlet weak var playerScore: UILabel!
+    @IBOutlet weak var cpuScore: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
